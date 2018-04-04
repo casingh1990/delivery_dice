bfaf08858ff3aa5237e95770d5f739556be8d303
Chandrashekar Singh <c.a.singh@hotmail.com>
2018-04-03 20:49:16 -0400

    Implemented 4 sided die

--- dice_rolling_test/lib/Models/D4.php
+++ dice_rolling_test/lib/Models/D4.php
@@ -2,7 +2,12 @@
 
 namespace DeliveryDotCom\Models;
 
-class D4
+class D4 extends AnyDie
 {
-
+  /**
+   * Constructor calls AnyDie's constructor to create a die with defined, default values for 10 sides
+   **/
+  function __construct(){
+    parent::__construct([1,2,3,4]);
+  }
 }
