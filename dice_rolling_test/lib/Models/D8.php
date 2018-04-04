b614c775b356b22f59845e2ad598cce2de70589c
Chandrashekar Singh <c.a.singh@hotmail.com>
2018-04-03 20:48:20 -0400

    Implemented 8 sided die

--- dice_rolling_test/lib/Models/D8.php
+++ dice_rolling_test/lib/Models/D8.php
@@ -2,7 +2,12 @@
 
 namespace DeliveryDotCom\Models;
 
-class D8
+class D8 extends AnyDie
 {
-
+  /**
+   * Constructor calls AnyDie's constructor to create a die with defined, default values for 10 sides
+   **/
+  function __construct(){
+    parent::__construct([1,2,3,4,5,6,7,8]);
+  }
 }
