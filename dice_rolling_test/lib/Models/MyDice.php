7ad3150d1d083783bd5c0288242f17908edba016
Chandrashekar Singh <c.a.singh@hotmail.com>
2018-04-03 20:49:55 -0400

    Implemented Dice container

--- dice_rolling_test/lib/Models/MyDice.php
+++ dice_rolling_test/lib/Models/MyDice.php
@@ -1,8 +1,30 @@
 <?php
 
 namespace DeliveryDotCom\Models;
+use DeliveryDotCom\Contracts\DiceContainerInterface;
+use DeliveryDotCom\Contracts\DiceInterface;
 
-class MyDice
+class MyDice implements DiceContainerInterface
 {
+  public $dices;
 
+  function __construct(){
+    $this->dices = [];
+  }
+
+  public function attach(DiceInterface $die){
+    $this->dices[] = $die;
+  }
+
+  /**
+   * Assuming that getTotal returns the sum of values of each die after each is rolled.
+   * @return Integer
+   **/
+  public function getTotal(){
+    $total = 0;
+    foreach($this->dices as $dice){
+      $total += $dice->roll();
+    }
+    return $total;
+  }
 }
