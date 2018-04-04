d056dd30b9c8c626945d7647224fb64a533ae5dd
Chandrashekar Singh <c.a.singh@hotmail.com>
2018-04-03 20:46:59 -0400

    Implemented 10 sided die

--- dice_rolling_test/lib/Models/D10.php
+++ dice_rolling_test/lib/Models/D10.php
@@ -2,7 +2,15 @@
 
 namespace DeliveryDotCom\Models;
 
-class D10
+/**
+ * A 10 sided die
+ **/
+class D10 extends AnyDie
 {
-
+  /**
+   * Constructor calls AnyDie's constructor to create a die with defined, default values for 10 sides
+   **/
+  function __construct(){
+    parent::__construct([1,2,3,4,5,6,7,8,9,10]);
+  }
 }
