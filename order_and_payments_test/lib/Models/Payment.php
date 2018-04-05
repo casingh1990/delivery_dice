2890333095708dc8f34c40c4ba8735aa30343a67
Chandrashekar Singh <c.a.singh@hotmail.com>
2018-04-05 11:12:26 -0400

    Payments - Added comments as well as some error checking on amounts along with Refund functionality

--- order_and_payments_test/lib/Models/Payment.php
+++ order_and_payments_test/lib/Models/Payment.php
@@ -8,29 +8,85 @@ use DeliveryDotCom\Contracts\PaymentInterface;
  **/
 class Payment implements PaymentInterface
 {
+  /**
+   * [CASH Payment]
+   * @var integer
+   */
   const CASH = 1;
+  /**
+   * [CREDIT_CARD Payment]
+   * @var integer
+   */
   const CREDIT_CARD = 2;
+  /**
+   * [REFUND Refund]
+   * @var integer
+   */
+  const REFUND = 3;
+  /**
+   * [private The amount in this payment]
+   * @var [Decimal]
+   */
   private $amount;
+  /**
+   * [private The type of payment - Must be one of the class constants above]
+   * @var [Integer]
+   */
   private $type;
+  /**
+   * [public Description attached to this payment. (Optional)]
+   * @var [String]
+   */
   public $description;
 
+  /**
+   * [__construct Class constructor]
+   * @param [decimal] $amount [Amount in this payment]
+   * @param [integer] $type   [Type of payment - Must be one of the above class constants]
+   */
   function __construct($amount, $type){
-    $this->amount = $amount;
+    //type must be set for amount validation`
     $this->setType($type);
+    $this->setAmount($amount);
   }
 
+  /**
+   * Sets the type of payment
+   * @param [Integer] $type [Type of payment - Must be one of the above class constants]
+   * @throws Exception
+   */
   public function setType($type){
-    if (($type === Payment::CASH) || ($type === Payment::CREDIT_CARD)){
+    if (($type === Payment::CASH) || ($type === Payment::CREDIT_CARD) || ($type === Payment::REFUND)){
       $this->type = $type;
     }
     else{
       throw new Exception("Type is invalid");
     }
   }
+
   /**
 	* @return int The amount of the individual payment
 	*/
 	public function getAmount(){
     return $this->amount;
   }
-}
+
+  /**
+   * Sets the amount. If it is a cash or credit card payment then the amount must be greater than zero.
+   *                  If it is a refund then the amount must be less than zero.
+   * @param [Decimal] $amount [the amount]
+   */
+  public function setAmount($amount){
+    if ( (($this->type === Payment::CASH) || ($this->type === Payment::CREDIT_CARD)) && ($amount > 0) ){
+      $this->amount = $amount;
+    }
+    else if ( ($this->type === Payment::REFUND) && ($amount < 0) ){
+      $this->amount = $amount;
+    }
+    else {
+      throw new Exception("Amount is invalid");
+
+    }
+  }
+
+}//end class
