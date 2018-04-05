d62a48c19799c95be84d4b50aee1970f875beee0
Chandrashekar Singh <c.a.singh@hotmail.com>
2018-04-04 21:12:28 -0400

    Added helper

---
+++ common/helper.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!function_exists("log_my_error")){
+  log_my_error($message){
+    if (!isset($CONFIG['logdir'])){
+      error_log("Log file configuration not set");
+    }
+    $logfile = $CONFIG["logdir"] . "/" . date('Ym') . ".log";
+    if (false === error_log($message, 3, $logfile)){
+      error_log("Error writing to log file");
+    }
+  }
+}
