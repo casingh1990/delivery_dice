42c297dacc4ec8cc02d376c9f5e05fa855a82eb6
Chandrashekar Singh <c.a.singh@hotmail.com>
2018-04-05 20:42:24 -0400

    Create log directory if does not exists

--- common/helper.php
+++ common/helper.php
@@ -5,6 +5,12 @@ if (!function_exists("custom_log")){
     if (!isset(CONFIG['logdir'])){
       error_log("Log file configuration not set");
     }
+    /**
+     * @todo The following might be refactored to throw an error instead
+     */
+    if (!file_exists(CONFIG['logdir'])) {
+      mkdir(CONFIG['logdir'], 0777, true);
+    }
     $logfile = CONFIG["logdir"] . "/" . date('Ym') . ".log";
     if (false === error_log($message, 3, $logfile)){
       error_log("Error writing to log file");
