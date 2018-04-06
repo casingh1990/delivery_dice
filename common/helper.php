44f48fd7ba540d95f190c2066ab44b5eded244cb
Chandrashekar Singh <c.a.singh@hotmail.com>
2018-04-05 20:29:50 -0400

    Fixed name of config variable

--- common/helper.php
+++ common/helper.php
@@ -1,11 +1,11 @@
 <?php
 
-if (!function_exists("log_my_error")){
-  function log_my_error($message){
-    if (!isset($CONFIG['logdir'])){
+if (!function_exists("custom_log")){
+  function custom_log($message){
+    if (!isset(CONFIG['logdir'])){
       error_log("Log file configuration not set");
     }
-    $logfile = $CONFIG["logdir"] . "/" . date('Ym') . ".log";
+    $logfile = CONFIG["logdir"] . "/" . date('Ym') . ".log";
     if (false === error_log($message, 3, $logfile)){
       error_log("Error writing to log file");
     }
